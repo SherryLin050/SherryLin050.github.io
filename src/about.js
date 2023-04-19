30c2f39dc33d374572d2e633b8b913b9af1005cc
SherryLin <tingpark@gmail.com>
2023-04-19 18:09:12 +0800

    test Vue

--- src/about.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-// 本頁css
-import './css/about.css'
-
-
--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { createApp } from 'vue'
-// 全域css
-import './css/style.css'
-import 'bootstrap';
-import 'https://code.jquery.com/jquery-3.6.4.min.js';
-import Header from './Header.vue'
-import Footer from './Footer.vue'
+import App from './App.vue'
 
-createApp(Header).mount('header')
-createApp(Footer).mount('footer')
+import './assets/main.css'
 
-window.addEventListener('load', function () {
-    var loading = document.getElementById('loading');
-    setTimeout(function() {
-      loading.style.display = 'none';
-    }, 1000); // 1秒後隱藏 loading 畫面
-});
+createApp(App).mount('#app')
